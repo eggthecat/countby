b051bd76b41945833dfcb7ea4d7bd9d6eec810d7
dominic montelongo and miatalksalot2019@gmail.com <dominic@>
2019-06-11 09:35:01 -0700

    Add JS material

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+//whenever our 'form' is "submitted" we perform a "function"
+  $ ('form').submit(function(event){
+    event.preventDefault();
+    var count = []
+  })
